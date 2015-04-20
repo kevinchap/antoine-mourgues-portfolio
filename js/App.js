f76c32000b8726655f081b13b074e0425551c053
kevinchap <kevin.chapron@hetic.net>
2015-04-20 14:19:36 +0200

    Corrections

--- js/App.js
+++ js/App.js
@@ -28,27 +28,19 @@ var AppRouter = Backbone.Router.extend({
     },
  
     home:function () {
-        home = true;
-
         setTimeout(function(){
             $('.home .menu-button, .home .title h1, .home .title .seperate, .home .title p, .home .scrolldown, .home .made-by').addClass('show');
         }, 4500);
 
         setTimeout(function(){
-            var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-            $(window).bind(mousewheelevt, function(e){
-                var evt = window.event || e //equalize event object     
-                evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
-                var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
-                if(delta < 0) {
-                    slide(true);
-                }
-            });
+            detectMouseWheel('home', true);
         }, 4500);
 
         $('.scrolldown').click(function(){
             slide(true);
         });
+
+        home = true;
     },
 
     projects:function () {
@@ -70,15 +62,7 @@ var AppRouter = Backbone.Router.extend({
             },2000);
         }
 
-        var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-        $('.portfolio .content .presentation').bind(mousewheelevt, function(e){
-            var evt = window.event || e //equalize event object     
-            evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
-            var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
-            if(delta > 0) {
-                slide(false);
-            }
-        });
+        detectMouseWheel('projects', false);
 
         if(projects !== true){
             this.projectList = new ProjectList();
@@ -99,9 +83,6 @@ tpl.loadTemplates(['project-list-item'], function() {
     app = new AppRouter();
     Backbone.history.start();
 });
- 
-// var app = new AppRouter();
-// Backbone.history.start();
 
 loadingSite();
 
@@ -125,6 +106,30 @@ function loadingSite(){
                 $("body").addClass("loaded");
             }, 2000);
         }
+    }
+}
+
+function detectMouseWheel(route, dir){
+    var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+    
+    if(route==='home'){
+        $(window).bind(mousewheelevt, function(e){
+            var evt = window.event || e //equalize event object     
+            evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+            var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+            if(delta < 0) {
+                slide(dir);
+            }
+        });
+    }else{
+        $('.portfolio .content .presentation').bind(mousewheelevt, function(e){
+            var evt = window.event || e //equalize event object     
+            evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+            var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+            if(delta > 0) {
+                slide(dir);
+            }
+        });
     }
 }
 
@@ -157,7 +162,7 @@ function showItemsProjects(){
     for(i=0;i<nbAlreadyInView;i++){
         setTimeout(function(lis,i){
             $(lis[i]).addClass('show');
-        },i*1800,lis,i);
+        },i*1200,lis,i);
     }
     for(i=nbAlreadyInView; i<lis.length; i++){
         $(lis[i]).bind('inview',function(event, isInView, visiblePartX, visiblePartY){
